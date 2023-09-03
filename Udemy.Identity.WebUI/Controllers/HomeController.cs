66b1a183d35c5b3138afb534e6ea3e41dc53b8d8
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-09-03 14:26:26 +0300

    Register işlemleri

--- Udemy.Identity.WebUI/Controllers/HomeController.cs
+++ Udemy.Identity.WebUI/Controllers/HomeController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Udemy.Identity.WebUI.Entities;
+using Udemy.Identity.WebUI.Models;
 
 namespace Udemy.Identity.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new()
+                {
+                    Email = model.EMail,
+                    UserName = model.UserName
+                };
+                var identityResult = await _userManager.CreateAsync(user, model.Password);
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");   
+                }
+
+                foreach (var item in identityResult.Errors)
+                {
+                    ModelState.AddModelError("",item.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Udemy.Identity.WebUI/Models/UserCreateModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Udemy.Identity.WebUI.Models
+{
+    public class UserCreateModel
+    {
+        [Required(ErrorMessage ="Kullanıcı adını giriniz")]
+        public string UserName { get; set; }
+        [EmailAddress(ErrorMessage ="Doğru formatta email girniz")]
+        [Required(ErrorMessage = "Email alanın giriniz")]
+
+        public string EMail { get; set; }
+
+        [Required(ErrorMessage = "Password")]
+
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Parolalar eşleşmiyor")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
+    
